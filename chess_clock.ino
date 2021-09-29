1f742f082033f309333d1639bf9287092f1d697d
Rafał Rudko <rafal.rudko@rb.com>
2021-09-29 19:53:04 +0200

    Add dummy logic for buttons, screen and millis (timer)

--- chess_clock.ino
+++ chess_clock.ino
@@ -10,25 +10,31 @@ void setup()
 {
     lcd.begin();
     lcd.backlight();
-     pinMode(l_button, INPUT_PULLUP);
-      pinMode(r_button, INPUT_PULLUP);
+    pinMode(l_button, INPUT_PULLUP);
+    pinMode(r_button, INPUT_PULLUP);
+    lcd.setCursor(0, 0);
+    lcd.print("Melina Krzemowa");
+    lcd.setCursor(5, 1);
+    lcd.print("CHESS CLOCK");
+    delay(2000);
+    lcd.clear();
 }
 
 void loop()
 {
-    if (digitalRead(l_button) == LOW) {
-      side = 0;
-    }
-    else if (digitalRead(r_button) == LOW) {
-      side = 1;
-    }
+  if (digitalRead(l_button) == LOW) {
+    side = 0;
+  }
+  else if (digitalRead(r_button) == LOW) {
+    side = 1;
+  }
 
-    if (side == 0) {
-      lcd.setCursor(0, 0);
-      lcd.print("left");
-    } else if (side ==1) {
-      lcd.print("right");
-    }
-    delay(100);
+  if (side == 0) {
+    lcd.setCursor(0, 0);
+    lcd.print(millis());
+  } else if (side ==1) {
     lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("right");
+  }
 }
